71bdfe759e91ec985da6f18da6769ff27d1a57d3
senjyouhara <ayano@loli.com.co>
2024-11-08 00:06:18 +0800

    update

--- Senjyouhara.Main/ViewModels/Components/MusicViewModel.cs
+++ Senjyouhara.Main/ViewModels/Components/MusicViewModel.cs
@@ -1,11 +1,28 @@
-﻿using PropertyChanged;
+﻿using System;
+using System.Collections.ObjectModel;
+using PropertyChanged;
 using Senjyouhara.Main.Core.Manager.Dialog;
+using Senjyouhara.Main.Model;
 
 namespace Senjyouhara.Main.ViewModels.Components;
 
 [AddINotifyPropertyChangedInterface]
 public class MusicViewModel : MyScreen
 {
+
+    public ObservableCollection<FileNameItem> FileNameItems { get; set; } = new()
+    {
+        new FileNameItem()
+        {
+            Uid = Guid.NewGuid().ToString(),
+            OriginFileName = "あたらよ-夏霞",
+            FileName = "あたらよ-夏霞",
+            FilePath = "E:\\KwDownload\\song\\あたらよ-夏霞.flac",
+            Suffix = "flac",
+            Uri = new Uri("E:\\KwDownload\\song\\あたらよ-夏霞.flac")
+        }
+    };
+    
     public bool IsLoadingTrack { get; set; }
     public bool ShowPause { get; set; }
 
